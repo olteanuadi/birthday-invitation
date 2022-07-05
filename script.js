7e221eed95bb0ab0cfa86ab2f6a531a2054b32d9
Adi <adisorin.olteanu@yahoo.com>
2022-07-05 21:13:07 +0300

    The duck seal now slides back when clicked 🐤

--- script.js
+++ script.js
@@ -3,13 +3,18 @@ const letter = document.querySelector(".letter");
 const letterSealClosed = document.querySelector(".letter-seal-closed");
 const letterSealOpened = document.querySelector(".letter-seal-opened");
 
+var toClick = true;
+
 function animation() {
+    if (toClick) {
     letter.classList.toggle("toggle");
-    duckSeal.classList.toggle("opac");
+    duckSeal.classList.toggle("move-ducky");
     letterSealClosed.classList.toggle("closed-seal-animation");
     letterSealOpened.classList.toggle("open-seal-animation");
 
     // letter.classList.toggle("opac");
+    toClick = false;
+    }
 }
 
-// duckSeal.addEventListener("click", animation);
+window.addEventListener("click", animation);
